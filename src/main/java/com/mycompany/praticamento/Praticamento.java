abb430c25eb1c4e9b98ee2edb5b31f9251bed6dc
Eric Bortoleto <ericbortoleto_9@outlook.com>
2024-04-11 16:08:28 -0300

    Entrega01(JornadaNasEstrelas): Implementa Tela de Login

---
+++ src/main/java/com/mycompany/praticamento/Praticamento.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.praticamento;
+
+/**
+ *
+ * @author Eric
+ */
+public class Praticamento {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! tudo bem?");
+    }
+}
